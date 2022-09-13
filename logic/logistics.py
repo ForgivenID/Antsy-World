080e51fb9783f4667109cfc85aad73a9c0b00858
Игнатов Иван <vanya.ignatow2012@yandex.ru>
2022-09-13 14:08:49 +0300

    logistics.py performance boost

--- logic/logistics.py
+++ logic/logistics.py
@@ -1,15 +1,22 @@
 import multiprocessing as mp
 import random as rn
+from functools import cache
 
 import misc.ProjSettings as settings
 import worldgen
 from logic.entities import BaseEntity, Ant
 from world import World
 
+@cache
+def get_cords_from_tiled(tiled_area):
+    return [(x, y) for x in range(tiled_area[0], tiled_area[2]) for y in
+     range(tiled_area[1], tiled_area[3])]
+
 
 class LogicProcess(mp.Process):
     def __init__(self, sim_settings, world_settings, manager):
         super(LogicProcess, self).__init__()
+        self.name = f"Simulation-{sim_settings.name}"
         self.world_settings = world_settings
         self.sim_settings = sim_settings
         self.tick = 0
@@ -26,15 +33,25 @@ class LogicProcess(mp.Process):
         for _ in range(100):
             data = self.generator.output.get()
             # print(data)
-            data[1]['entities'] = []
+            data[1]['entities'] = {}
             self.world.rooms_data[data[0]] = data[1]
-        self.entities = [Ant((rn.randint(0, settings.RoomSettings.dimensions[0]),
-                              rn.randint(0, settings.RoomSettings.dimensions[1])),
-                             self.world.rooms_data[rn.choice(list(self.world.rooms_data.keys()))]) for _ in range(10)]
+        self.entities = []
+        for _ in range(100):
+            cords = (rn.randint(0, settings.RoomSettings.dimensions[0]),
+                 rn.randint(0, settings.RoomSettings.dimensions[1]))
+            room = self.world.rooms_data[rn.choice(list(self.world.rooms_data.keys()))]
+            if cords in room['tiles'] and room['tiles'][cords]['object'] == 'NormalFloor':
+                self.entities.append(Ant(cords, room))
+        counter = 0
         while not self.manager.halted.is_set():
+            counter += 1
             self.update()
+            if not counter % 1000:
+                print('1')
+                self.world.save()
         self.generator.halt()
         self.generator.join(timeout=3)
+        self.world.save()
         print('ended')
 
     def update(self):
@@ -42,11 +59,9 @@ class LogicProcess(mp.Process):
             data = self.generator.output.get()
             # print(data)
             self.world.rooms_data[data[0]] = data[1]
-        for entity in self.entities:
-            entity.update(self.tick)
+        [entity.update(self.tick) for entity in self.entities]
         tiled_area = self.manager.get_camera_boundaries()
-        cords = [(x, y) for x in range(tiled_area[0], tiled_area[2]) for y in
-                 range(tiled_area[1], tiled_area[3])]
+        cords = get_cords_from_tiled(tiled_area)
         # print(tiles) if tiles != [] else None
         self.manager.set_rooms(self.get_rooms(cords))
         self.tick += 1
